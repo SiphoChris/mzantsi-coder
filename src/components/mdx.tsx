ee05bc3c2ec3336ff6c96d8902b295dc2bc414b9
sinkecpt025 <sinkecpt025@student.wethinkcode.co.za>
2026-04-16 14:36:07 +0200

    fix: accordion component resolution - add it in mdx.tsx

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -1,15 +1,42 @@
+// import defaultMdxComponents from 'fumadocs-ui/mdx';
+// import type { MDXComponents } from 'mdx/types';
+
+// export function getMDXComponents(components?: MDXComponents) {
+//   return {
+//     ...defaultMdxComponents,
+//     ...components,
+//   } satisfies MDXComponents;
+// }
+
+// export const useMDXComponents = getMDXComponents;
+
+// declare global {
+//   type MDXProvidedComponents = ReturnType<typeof getMDXComponents>;
+// }
+
+
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
+
+import {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent,
+} from '@/components/ui/accordion';
 
 export function getMDXComponents(components?: MDXComponents) {
   return {
     ...defaultMdxComponents,
+
+    // custom components here
+    Accordion,
+    AccordionItem,
+    AccordionTrigger,
+    AccordionContent,
+
     ...components,
   } satisfies MDXComponents;
 }
 
 export const useMDXComponents = getMDXComponents;
-
-declare global {
-  type MDXProvidedComponents = ReturnType<typeof getMDXComponents>;
-}
